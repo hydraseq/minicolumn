6532c85145f0e8d9812dfdd5661aa825aba57e69
EfrainOlivaresCD <efrain.olivares@carpe.io>
2020-05-17 02:55:31 -0700

    playing with vocab

--- chatbot.py
+++ chatbot.py
@@ -2,5 +2,8 @@ from minicolumn import MiniColumn
 
 mcol = MiniColumn(['chat.0.txt', 'chat.1.txt', 'chat.2.txt'], "tests/data")
 
-print(mcol.get_hits("good morning"))
+sent = "read junk"
+print(mcol.evaluate(sent))
+print("-------------")
+print(mcol.get_hits(sent))
 
